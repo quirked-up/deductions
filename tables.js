d4da18ae1a3fedda06fdaa67da3cf6b17d5a794e
quirked-up <115190871+quirked-up@users.noreply.github.com>
2023-04-19 12:40:32 +1200

    add connective tables, with disabling & custom inputs

--- tables.js
+++ tables.js
@@ -1,6 +1,11 @@
+// TODO: make deleting variables work
+// rename to more sensible names
+// n-ary predicates
+// functions
+// second order quantifiers
 const CURRENT = {}
 const TEMPLATES = {}
-function update_current(type, template_num) {
+function update_current(type, template_num, initialisation=false) {
 	if (isNaN(template_num)) {
 		// custom
 		TEMPLATES[type].values[-1] = [...document.querySelectorAll(`input[name="${type}"][value=custom]~fieldset input`)].map(input => input.value);
@@ -14,6 +19,8 @@ function update_current(type, template_num) {
 	for (const [i, val] of Object.entries(CURRENT[type]))
 		document.body.style.setProperty(`--${type}-${i}`, JSON.stringify(val||" "));
 
+	if (!initialisation)
+		update_connective_tables()
 }
 function add_template(type, template_name, values) {
 	TEMPLATES[type].names.push(template_name)
@@ -24,13 +31,13 @@ add_template("CONNECTIVES", "Modern Logic", ['¬', '∧', '∨', '⊻', '→', '
 add_template("CONNECTIVES", "Traditional Logic", ['~', '∧', '∨', '⊻', '⊃', '≡', "∀", "∃"])
 add_template("CONNECTIVES", "Programming", ['!', '&', '|', '^', '→', '==', "∀", "∃"])
 add_template("CONNECTIVES", "Algebra", ['¬', '•', '+', '⊕', '→', '≡', "∀", "∃"])
-update_current("CONNECTIVES", 0)
+update_current("CONNECTIVES", 0, true)
 
 TEMPLATES["TRUE_FALSE_SYMBOLS"] = {name: "Truth Symbols", hide_fields:true, fields: 2, names: [], values: []}
 add_template("TRUE_FALSE_SYMBOLS", "T/F", ['F', 'T', ''])
 add_template("TRUE_FALSE_SYMBOLS", "1/0", ['0', '1', ''])
 add_template("TRUE_FALSE_SYMBOLS", "⊤/⊥", ['⊥', '⊤', ' '])
-update_current("TRUE_FALSE_SYMBOLS", 0)
+update_current("TRUE_FALSE_SYMBOLS", 0, true)
 
 class Token {
 	constructor(string, stack=[], override_type=false) {
@@ -44,6 +51,7 @@ class Token {
 			var longest_match = 0
 			type = null
 			for (let i=0; i<TYPES.length; i++) {
+				if (TYPES[i].disabled) continue
 				for (let j=0; j<TYPES[i].aliases.length; j++) {
 					if (TYPES[i].aliases[j].length > longest_match && string.startsWith(TYPES[i].aliases[j])) {
 						type = TYPES[i]
@@ -127,8 +135,9 @@ class Token {
 
 const TYPES = []
 class TokenType {
-	constructor(name, symbols='', {post_args=()=>0, pre_args=()=>0, args, valuate=()=>INDETERMINATE, display=false, quantifier=false, index} = {}) {
+	constructor(name, symbols='', {post_args=()=>0, pre_args=()=>0, args, valuate=()=>INDETERMINATE, display=false, quantifier=false, index, disabled} = {}, fullname) {
 		this.name = name
+		this.fullname = fullname
 		this.aliases = typeof(symbols)=="string" ? symbols.split('') : symbols;
 		if (args) {
 			this.pre_args  = s=>args(s)[0]
@@ -142,6 +151,7 @@ class TokenType {
 			: function(_) { return this.args.reduce((vals,arg)=>vals[+arg.valuate(_)], valuate) }  // truth table [[0, 1], [1, 1]] -> function args[0].valuate() || args[1].valuate()
 		this.quantifier = quantifier
 		this.index = index || function(_){return this.type.aliases.indexOf(this.symbol)}
+		this.disabled = disabled
 		TYPES.push(this)
 	}
 }
@@ -156,20 +166,16 @@ const relation_argcounts = []
 const RELATIONS = new TokenType("relation", "", {args: s=> relation_argcounts[RELATIONS.aliases.indexOf(s)], 
 	valuate: function(struct){ return this.args.reduce((t,c)=>t[c.index(struct)], struct[1][this.index(struct)]) } })
 new TokenType("negation", ['!', '~', '¬', '\\lnot', 'NOT'], {post_args:1, display:['CONNECTIVES',0], 
-	valuate: [1,0,INDETERMINATE] })
+	valuate: [1,0,INDETERMINATE] }, "Negation (NOT)")
 new TokenType("disjunction", ['v', '∨', '|', '||', '+', '\\lor', 'OR', '\\/'], {pre_args:1, post_args:1, display:['CONNECTIVES',2], 
-	valuate: [[0,1,INDETERMINATE],[1,1,1],[INDETERMINATE,1,INDETERMINATE]] })
+	valuate: [[0,1,INDETERMINATE],[1,1,1],[INDETERMINATE,1,INDETERMINATE]] }, "Inclusive Disjunction (OR)")
 new TokenType("conjunction", ['&', '∧', '^', '&&', '•', '.', '\\land', 'AND', '/\\'], {pre_args:1, post_args:1, display:['CONNECTIVES',1], 
-	valuate: [[0,0,0],[0,1,INDETERMINATE],[0,INDETERMINATE,INDETERMINATE]] })
+	valuate: [[0,0,0],[0,1,INDETERMINATE],[0,INDETERMINATE,INDETERMINATE]] }, "Conjunction (AND)")
 new TokenType("implication", ['>', '→', '⊃', '\\implies', '->', 'IMPLIES'], {pre_args:1, post_args:1, display:['CONNECTIVES',4], 
-	valuate: [[1,1,1],[0,1,INDETERMINATE], [INDETERMINATE,1,INDETERMINATE]] })
+	valuate: [[1,1,1],[0,1,INDETERMINATE], [INDETERMINATE,1,INDETERMINATE]] }, "Material Implication")
 new TokenType("bi-implication", ['<>', '<->', '=', '==', '\\leftrightarrow', 'IFF'], {pre_args:1, post_args:1, display:['CONNECTIVES',5],
-	valuate: [[1,0,INDETERMINATE],[0,1,INDETERMINATE], [INDETERMINATE,INDETERMINATE,INDETERMINATE]] })
+	valuate: [[1,0,INDETERMINATE],[0,1,INDETERMINATE], [INDETERMINATE,INDETERMINATE,INDETERMINATE]] }, "Equivalence")
 new TokenType("postfix operator", "'", {pre_args:1, valuate:function(_){return this.args[0].valuate(_)}})
-new TokenType("falsum", ['#','F','⊥','0','\\bot'], {display:['TRUE_FALSE_SYMBOLS',0],
-	valuate: false })
-new TokenType("verum", ['T','1','⊥','\\top'], { display:['TRUE_FALSE_SYMBOLS',1],
-	valuate: true })
 new TokenType("universal_quantifier", ["A", "∀"], {post_args:2, display:["CONNECTIVES",6], quantifier: "VARIABLES",
 	valuate: function ([cm,rm,quantifier_choices]) { 
 		let indetermined = false
@@ -179,7 +185,7 @@ new TokenType("universal_quantifier", ["A", "∀"], {post_args:2, display:["CONN
 			if (val == INDETERMINATE) indetermined = true
 		}
 		return indetermined ? INDETERMINATE : true
-	} })
+	} }, "Universal Quantifier")
 new TokenType("existential_quantifier", ["E", "∃"], {post_args:2, display:["CONNECTIVES",7], quantifier: "VARIABLES",
 	valuate: function ([cm,rm,quantifier_choices]) { 
 		let indetermined = false
@@ -189,10 +195,14 @@ new TokenType("existential_quantifier", ["E", "∃"], {post_args:2, display:["CO
 			if (val == INDETERMINATE) indetermined = true
 		}
 		return indetermined ? INDETERMINATE : false
-	} })
+	} }, "Existential Quantifier")
+new TokenType("falsum", ['#','F','⊥','0','\\bot'], {display:['TRUE_FALSE_SYMBOLS',0],
+	valuate: false }, "False / Falsum / Contradiction")
+new TokenType("verum", ['T','1','⊥','\\top'], { display:['TRUE_FALSE_SYMBOLS',1],
+	valuate: true }, "Truth / Verum")
 const CONSTANTS = new TokenType("constant", "", {
 	valuate: function([const_map]){return const_map[this.symbol]} })
-const BINDING_VARIABLE = new TokenType("binding_variable")
+const BINDING_VARIABLE = new TokenType("binding_variable", "", {disabled:true})
 
 function scan(txt) {
 	var stack = []
@@ -491,7 +501,6 @@ UNARY_PREDICATE_TABLE.addEventListener('input', ({target, data})=>{
 	if (target.type == "checkbox") {
 		const tr = target.parentElement.parentElement;
 		const num_columns = tr.childElementCount - 2; // -header -spacingcolumn
-		console.log(tr.children)
 		var num_rows = tr.parentElement.childElementCount;
 		
 		const [_, row, col] = target.id.split('-');
@@ -650,7 +659,6 @@ function handle_bpt_input({target}) {
 		if (target.innerText.includes('\n')) // only trim if will make a difference, because it also resets the cursor
 			target.innerText = target.innerText.replaceAll('\n', '') // BUG: space at end is c
 
-		z = target
 		console.log(Array.prototype.indexOf.call(BPT_CONTAINER.children, target.parentElement.parentElement.parentElement))
 		RELATIONS.aliases[unary_pred_count + Array.prototype.indexOf.call(BPT_CONTAINER.children, target.parentElement.parentElement.parentElement)] = target.innerText
 	}
@@ -665,3 +673,61 @@ function set_var_name(index, newName) {
 		table.tBodies[0].rows[index].cells[0].innerText = newName
 	}
 }
+
+/* add connective tables */
+const CONNECTIVE_CONTAINER = document.getElementById('connectives')
+for (const connective of TYPES.filter(x=>x.custom_display))
+	CONNECTIVE_CONTAINER.appendChild(create_connective_table(connective))
+function create_connective_table(connective) {
+	const arg_names = 'pqrstuvwxyz'.split('')
+	const total_args = connective.pre_args() + connective.post_args()
+
+	const table = document.createElement('table')
+	table.classList.add('true-false-grid')
+
+	connective.args = [] // so we can call .valuate()
+	console.log(total_args);
+	table.innerHTML = `<caption>
+		<h3>${connective.fullname}</h3>
+		<label><input type=checkbox checked> Enable</label><br>
+		<label>Inputs: <input value="${connective.aliases.join(', ').replaceAll('"', '&qt;')}"></label>
+	</caption>
+	${connective.quantifier ? '' : `<thead>
+		${arg_names.slice(0,total_args).map(x=>`<th>${x}</th>`).join('')}
+		<td></td>
+		<th>
+			${arg_names.slice(0,connective.pre_args()).join('')}
+			<span>${CURRENT[connective.custom_display[0]][connective.custom_display[1]]}</span>
+			${arg_names.slice(connective.pre_args(), total_args).join('')}
+		</th>
+	</thead>
+	${Array(2**total_args).fill(0).map((_,i)=>i.toString(2).padStart(total_args, '0')).map(args=>`<tr>
+		${Array(total_args).fill(0).map((_,i)=>`<th>
+			<input disabled type=checkbox ${args[i] == "1" ? 'checked' : ''}><label></label>
+		</th>`).join('')}
+		<td></td>
+		<td><input disabled type=checkbox ${(total_args==0 || (connective.args = args.split('').map(x=>{return {valuate: _=>+x}}))) && connective.valuate({}) ? 'checked' : ''}><label></label></td>
+	</tr>`).join('')}
+	`}`
+	connective.args = undefined // meaningless property, only useful for this. reset to reduce confusion
+
+	table.addEventListener('input', ({target})=>{
+		if (target.type == 'checkbox') // toggle enable
+			connective.disabled = !target.checked
+		else // inputs
+			connective.aliases = target.value.split(',').map(x=>x.trim())
+		updateFormula()
+	})
+
+	table.dataset.symb_type = connective.custom_display[0]
+	table.dataset.symb_indx = connective.custom_display[1]
+
+	return table
+}
+function update_connective_tables() {
+	for (const table of CONNECTIVE_CONTAINER.children) {
+		if (table.tHead)
+			table.tHead.rows[0].lastElementChild.firstElementChild.innerText = CURRENT[table.dataset.symb_type][table.dataset.symb_indx]
+	}
+}
+// TODO: add ability to create connectives
